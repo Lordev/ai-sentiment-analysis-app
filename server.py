4d835385a3dafed8cc94123cc8c63978e6049e8c
lordev <lorenzo.sallons.dev@gmail.com>
2025-06-25 17:40:25 -0400

    feat: implemented error handling for blank form input

--- server.py
+++ server.py
@@ -11,13 +11,17 @@ def index():
 def sent_emotion_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+    
+    if response is None or response.get('dominant_emotion') is None:
+        return "Invalid text! Please try again!"
+
     return (
         "For the given statement, the system response is:\n"
-        f"  anger: {response['anger']:.3f},\n"
-        f"  disgust: {response['disgust']:.3f},\n"
-        f"  fear: {response['fear']:.3f},\n"
-        f"  joy: {response['joy']:.3f},\n"
-        f"  sadness: {response['sadness']:.3f}.\n"
+        f"  anger: {response['anger']:.3f}\n"
+        f"  disgust: {response['disgust']:.3f}\n"
+        f"  fear: {response['fear']:.3f}\n"
+        f"  joy: {response['joy']:.3f}\n"
+        f"  sadness: {response['sadness']:.3f}\n"
         f"  The dominant emotion is: {response['dominant_emotion']}"
     )
 
